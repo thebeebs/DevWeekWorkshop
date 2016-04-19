a53bb0dc6081b9d392a6b15bd798f38572efe052
thebeebs <martinbeeby@hotmail.com>
2016-04-19 12:01:50 +0100

    Changes to Promises FIle

--- promise.js
+++ promise.js
@@ -1,25 +1,38 @@
 'use strict';
+addElementOld("First");
+addElementOld("Second");
+addElementOld("Third");
+addElementOld("Fourth");
+
 // addElement("first")
 // .then(x => {return addElement("second")})
 // .then(x => { return addElement("third")})
 // .then(x => { return addElement("fourth")})
 
-doThisStuff();
- async function doThisStuff(){
-   await addElement("first");
-   await addElement("second");
-   await addElement("third");
-   await addElement("fourth");  
-}
+// doThisStuff();
+//  async function doThisStuff(){
+//    await addElement("first");
+//    await addElement("second");
+//    await addElement("third");
+//    await addElement("fourth");  
+// }
 
 function addElement(elementText){
-    var myPromise = new Promise(function(resolve,reject){
-    setTimeout(function(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            var element=document.createElement('H1');
+            element.innerText = `${elementText} ${Date.now()}`;
+            document.body.appendChild(element);
+            resolve();
+        }, Math.random() * 2000);
+    });
+}
+
+function addElementOld(elementText){
+     setTimeout(function(){
         var element=document.createElement('H1');
         element.innerText = `${elementText} ${Date.now()}`;
         document.body.appendChild(element);
-        resolve();
     }, Math.random() * 2000);
-});
-return myPromise;}
+}
 
